faa9ac7bfbd2429aa8c51d19daf7417aa91568d6
Burgess, Mark (H&B, Black Mountain) <Mark.A.Burgess@csiro.au>
2024-06-17 10:27:39 +1000

    initial push of synthetic mouse data

--- experiment/pharmacogenetic/script.sh
+++
@@ -1,3 +0,0 @@
-genomator ../sources/ALL.chr10_and_16.cleaned.vcf.pickle GENOMATOR_.pickle 1000 1 1
-pickle_to_vcf.py GENOMATOR_.pickle ../sources/ALL.chr10_and_16.cleaned.vcf GENOMATOR_.vcf
-stochastic_sanity_checker.py  ../sources/ALL.chr10_and_16.cleaned.vcf GENOMATOR_.vcf
--- experiment/quadruplets/analyse.py
+++
@@ -1,6 +0,0 @@
-import glob
-import os
-datasetfiles = glob.glob("*.vcf")
-reffiles = ["../sources/AGBL4.vcf.gz"]*len(datasetfiles)
-input_files = sum(list(map(list,zip(datasetfiles,reffiles))),[])
-os.system("rare_SNP_diagnosis8.py {} --degree=4 --trials=10000 > results.txt".format(" ".join(input_files)) )
