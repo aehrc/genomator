9ef812b5f12e11e5ac8ccb74e57139868a4f47c3
Brendan Hosking <brendan.hosking@csiro.au>
2024-06-19 01:58:51 +0000

    FEAT: Add runtime test Now the genomator binary is included, we can compute run times on the fly.
    Calculate run times of genomator as an additional test. Don't yet compute run times of other methods, as we are still excluding them from the capsule.

---
+++ code/tests/runtime.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -exuo pipefail
+INPUT_VCF_DIR=/data/pickles/
+INPUT_FILE_PREFIX=chr_filtered_
+INPUT_VCF_SUFFIX=.vcf.gz.pickle
+TAR_SUFFIX=.tar.gz
+
+
+GENOMATOR () {
+    \time --format=%U --output=$2 genomator $1 dummy_output.pickle 1 1 1
+}
+MARKOV () {
+    \time --format=%U --output=$2 MARK_run.py $1 dummy_output.pickle 1 --window_leng=30
+}
+GAN () {
+    \time --format=%U --output=$2 GAN_run.py $1 dummy_output 1 --dump_output_interval=30000 --epochs=300
+}
+RBM () {
+    \time --format=%U --output=$2 RBM_run.py $1 dummy_output 1 --dump_output_interval=30000 --gpu=True --ep_max=1200
+}
+CRBM () {
+    \time --format=%U --output=$2 CRBM_run.py $1 dummy_output 1 --dump_output_interval=30000 --gpu=True --ep_max=1200
+}
+
+RESULTS_DIR=$1
+RESULTS_FILE="${RESULTS_DIR}/runtime_results.tsv"
+TEMP_RUNTIME_FILE=runtime.txt
+
+echo -e "Method\tSNPs\tTime(s)" > $RESULTS_FILE
+for alg in GENOMATOR MARKOV GAN RBM CRBM; do
+    if [[ "${alg}" == "GENOMATOR" ]]; then
+        for i in 100 400 1600 6400 25600 102400 409600 1638400 6553600 11757483; do
+            input_vcf="${INPUT_FILE_PREFIX}${i}${INPUT_VCF_SUFFIX}"
+            tar -xzf "${INPUT_VCF_DIR}${input_vcf}${TAR_SUFFIX}" --no-same-owner
+            $alg $input_vcf $TEMP_RUNTIME_FILE
+            echo -en "${alg}\t${i}\t" >> $RESULTS_FILE
+            cat $TEMP_RUNTIME_FILE >> $RESULTS_FILE
+            rm $input_vcf
+        done
+    else
+        continue
+    fi
+done
