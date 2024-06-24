ee90495edc53c0a0d115082991498d5adf2b02ef
Brendan Hosking <brendan.hosking@csiro.au>
2024-06-24 00:24:51 +0000

    REFACTOR: Remove unused scripts
    Some old versions of the scripts for the runtime and pharmacogenetic tests are still stored in the repo. Since we are unable to run the competing methods in Code Ocean, there is little point in producing a runtime graph. The pharmacogenetic script has been refactored and placed into /code/tests.
    
    Remove the old and unused runtime and pharmacogenetic scripts.

--- code/pharmacogenetic/script.sh
+++
@@ -1,5 +0,0 @@
-# NOTE: THIS EXPERIMENT TOO BIG FOR CODEOCEAN
-
-#genomator ../sources/ALL.chr10_and_16.cleaned.vcf.pickle GENOMATOR_.pickle 1000 1 1
-#pickle_to_vcf.py GENOMATOR_.pickle ../sources/ALL.chr10_and_16.cleaned.vcf GENOMATOR_.vcf
-#stochastic_sanity_checker.py  ../sources/ALL.chr10_and_16.cleaned.vcf GENOMATOR_.vcf
--- code/runtimes/script.sh
+++
@@ -1,4 +0,0 @@
-# NOTE: THIS EXPERIMENT IS TOO BIG FOR CODEOCEAN
-
-#python run_script.py
-#python produce_graph.py
