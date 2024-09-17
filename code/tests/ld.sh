8e9b417fc486afa89738a9ecf589a181f5f06e86
Brendan Hosking <brendan.hosking@csiro.au>
2024-09-17 17:30:35 +1000

    FIX: Reduce number of generated genomes in ld test

--- code/tests/ld.sh
+++ code/tests/ld.sh
@@ -7,21 +7,21 @@ from_vcfshark () {
 }
 
 GENOMATOR () {
-    genomator $1 $2 2000 1 1
+    genomator $1 $2 1000 1 1
 }
 MARK () {
-    MARK_run.py $1 $2 2000 --window_leng=10
+    MARK_run.py $1 $2 1000 --window_leng=10
 }
 GAN () {
-    GAN_run.py $1 gan 2000 --dump_output_interval=20000 --epochs=20010
+    GAN_run.py $1 gan 1000 --dump_output_interval=20000 --epochs=20010
     pickle_to_vcf.py gan20000.pickle $1 $2
 }
 RBM () {
-    RBM_run.py $1 rbm 2000 --dump_output_interval=1200 --gpu=True --ep_max=1250
+    RBM_run.py $1 rbm 1000 --dump_output_interval=1200 --gpu=True --ep_max=1250
     pickle_to_vcf.py rbm1201.pickle $1 $2
 }
 CRBM () {
-    CRBM_run.py $1 crbm 2000 --dump_output_interval=1200 --gpu=True --ep_max=1250
+    CRBM_run.py $1 crbm 1000 --dump_output_interval=1200 --gpu=True --ep_max=1250
     pickle_to_vcf.py crbm1201.pickle $1 $2
 }
 
