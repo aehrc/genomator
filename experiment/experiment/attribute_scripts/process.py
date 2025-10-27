7370de39231b170c84842c069d7200c8eeeb0bb5
Burgess, Mark (H&B, Black Mountain) <Mark.A.Burgess@csiro.au>
2025-10-27 15:20:56 +1100

    updating attribute experiment for appropriate eps image output

--- experiment/experiment/attribute_scripts/process.py
+++ experiment/experiment/attribute_scripts/process.py
@@ -21,6 +21,11 @@ markers = ['.','v','s','P','*','d','X']*5
 plt.figure()
 keys = sorted(converted_data.keys())
 keys.remove("x")
+
+
+vert_x = converted_data['x'][0][0]
+plt.gca().add_patch(patches.Rectangle((vert_x,-0.0001),-0.0,0.0027, linewidth=1, edgecolor="#67f9f8", facecolor="none"))
+
 for i,k in enumerate(keys):
     xs,ys = list(zip(*converted_data[k]))
     ys = [ys[i] - xs[i] for i in range(len(ys))]
@@ -30,15 +35,34 @@ for i,k in enumerate(keys):
             c=[base_colours[i]],
             label=k.capitalize(),
             alpha=0.8,
-            marker=markers[i])
+            marker=markers[i],
+            linewidth=0)
 
-plt.gca().add_patch(patches.Rectangle((0.1750,0.0006),0.015,0.0005, linewidth=1, edgecolor="black", facecolor="none"))
-vert_x = converted_data['x'][0][0]
-plt.gca().add_patch(patches.Rectangle((vert_x,0.0),-0.0,0.0025, linewidth=1, edgecolor=base_colours[4], facecolor="none"))
-#plt.gca().add_patch(patches.Rectangle((0.17,0.0),0.06,0.0, linewidth=1, edgecolor="black", facecolor="none"))
+# OPTIONAL FOR SHOWING REGION OF PARAMETER PERTURBATION
+#plt.gca().add_patch(patches.Rectangle((0.1750,0.0006),0.015,0.0005, linewidth=1, edgecolor="black", facecolor="none"))
+
+highlighted_points = [
+(0.18132295719844357,
+0.18197909514000152),
+(0.22410925459678036,
+0.22530708781567102),
+(0.18689250019073778,
+0.18754863813229572),
+(0.18958571755550468,
+0.18987563897154192)
+]
+
+plt.scatter([a for a,b in highlighted_points],[b-a for a,b in highlighted_points],s=58,marker='o', facecolor='none', edgecolor='black', linewidth=0.5)
+
 
 lgnd = plt.legend(loc="upper right", scatterpoints=1, fontsize=10)
 plt.xlabel("In-data distance")
 plt.ylabel("Out-data distance minus In-data distance")
-plt.show()
-#plt.savefig("results.png", bbox_inches='tight')
+
+plt.xlim(0.169, 0.23) # Limit x-axis from -5 to 5
+plt.ylim(-0.0001, 0.0026) # Limit y-axis from -1 to 1
+
+
+#plt.show()
+plt.savefig("results_attribute_experiment.png", bbox_inches='tight')
+plt.savefig("results_attribute_experiment.eps", format='eps', dpi=300) 
